2775f7b2f1db8ad0097334f60f3296640625b8e2
Egor Smirnov <egorsmir@gmail.com>
2016-06-07 13:49:20 +0200

    feat(reducers): add initial state to location reducer

--- reducers/location.js
+++ reducers/location.js
@@ -1,4 +1,9 @@
-export function location(state = {}, action) {
+const initialState = {
+    latitude: 55.751244,
+    longitude: 37.618423
+};
+
+export function location(state = initialState, action) {
     switch (action.type) {
         
         default:
