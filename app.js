cb764cf2a2ff2a04e8597d533df90ca0e561c878
Egor Smirnov <egorsmir@gmail.com>
2016-06-07 13:03:15 +0200

    fix(Root): fix the problem with undefined Provider

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './containers/App';
+import { Provider } from 'react-redux'
+import Root from './containers/Root';
 import configureStore from './store/configureStore';
 
 const store = configureStore();
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <Root />
     </Provider>,
     document.querySelector('.root')
 );
